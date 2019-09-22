9e0491679d717ba61bf679ff97adc803ff793d83
Zapel <lazebaoleg@gmail.com>
2019-09-22 08:48:55 +0300

    add 5_coroutines.py

---
+++ 5_coroutines.py
@@ -0,0 +1,35 @@
+
+def coroutine(func):
+    def inner(*args, **kwargs):
+        g = func(*args, **kwargs)
+        g.send(None)
+        return g
+    return inner
+
+@coroutine
+def subgen():
+    x = 'Ready to accept message'
+    message = yield x
+    print('Subgen received:', message)
+
+class BlaBlaException(Exception):
+    pass
+
+@coroutine
+def average():
+    count = 0
+    sum = 0
+    average = None
+
+    while True:
+        try:
+            x = yield average
+        except StopIteration:
+            print('Done')
+        except BlaBlaException:
+            print('..................................')
+        else:
+            count += 1
+            sum += x
+            average = round(sum/count, 2)
+
