990498f6dadf5d1a75a693041f64afcb60ccb41d
Zapel <lazebaoleg@gmail.com>
2019-09-24 00:53:17 +0300

    change write_dic_cvs.py

--- cvs/write_dic_cvs.py
+++ cvs/write_dic_cvs.py
@@ -2,26 +2,37 @@ import csv
 
 FILENAME = "/home/oleg/git/asynchronous_python/cvs/files/users_dic.csv"
 
-users = [
-    {"name": "Tom", "age": 28},
-    {"name": "Alice", "age": 23},
-    {"name": "Bob", "age": 34}
-]
+users = []
+columns = ["name", "age", "country", "city"]
+user = {"name": "Sam", "age": 41, "country": "UA", "city": "Kiev"}
+print('users_before: ', users)
+
+dic1 = {"key1": "Tom", "key2": 28}
+dic2 = {"key1": "Alice", "key2": 23}
+
+inner_list = []
+# print('inner_list_before: ', inner_list)
+inner_list.append(dic1)
+inner_list.append(dic2)
+# print('inner_list_after: ', inner_list)
+
+for i in inner_list:
+    x = {}
+    user["name"] = i["key1"]
+    user["age"] = i["key2"]
+    x.update(user)
+    users.append(x)
+print('users_after: ', users)
 
 with open(FILENAME, "w", newline="") as file:
-    columns = ["name", "age"]
-    writer = csv.DictWriter(file, fieldnames=columns)
+
+    writer = csv.DictWriter(file, delimiter=';', fieldnames=columns)
     writer.writeheader()
-
-    # запись нескольких строк
+    # writer.writerow(user)
     writer.writerows(users)
-
-    user = {"name": "Sam", "age": 41}
-    # запись одной строки
-    writer.writerow(user)
 
 with open(FILENAME, "r", newline="") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        print(row["name"], "-", row["age"])
+        print(row.values())
 
---
+++ test.py
@@ -0,0 +1,7 @@
+list = []
+
+for i in range(10):
+    list.append({1: 25})
+    print(list)
+
+print(list)
